f3931d15f6a17a1a44cfb43d32955175e1b0faa2
burugi <pinkbin4566@gmail.com>
2020-01-18 11:30:34 +0900

    Paint JS ver 0.0.6
    canvas 브러쉬의 색깔변경

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
-
+//canvas multiplier에게 값이 주어져있지 않아서 그려지지 않앗다.
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -29,13 +30,18 @@ function onMouseMove(event) {
   }
 }
 //offset이 캔버스안의 내용이다. client 사이즈는 윈도우의 커서위치
-function onMouseDown(event) {
-  painting = true;
-}
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
-  canvas.addEventListener("mouseleave", onmouseLeave);
+  canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);

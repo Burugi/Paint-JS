4e88172d67625d51a754474f5e081855032b50f3
burugi <pinkbin4566@gmail.com>
2020-01-18 11:08:43 +0900

    Paint JS ver 0.0.5
    canvas안에 선 그리기표현

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
-let painting = fasle;
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
 
 function stopPainting(event) {
-  painting = fasle;
+  painting = false;
 }
+
+function startPainting() {
+  painting = true;
+}
+
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 //offset이 캔버스안의 내용이다. client 사이즈는 윈도우의 커서위치
 function onMouseDown(event) {
   painting = true;
 }
 
-function onMouseUp(event) {
-  stopPainting();
-}
-
-function onmouseLeave(event) {
-  painting = false;
-}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", onmouseLeave);
 }

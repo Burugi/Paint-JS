b7fc13019028a7ede7a83576400d932e48f71e0e
burugi <pinkbin4566@gmail.com>
2020-01-18 12:10:47 +0900

    Paint JS ver 1.0.0
    canvas의 png저장 그리고 마무리

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
-
+const saveBtn = document.getElementById("jsSave");
 const INITIAL_COLOR = "#2C2C2C";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 //canvas multiplier에게 값이 주어져있지 않아서 그려지지 않앗다.
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+//배경을 white로 지정해줘야한다.
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -52,12 +56,25 @@ function handleCanvasCilick() {
     ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   }
 }
+
+function handleCM(event) {
+  event.preventDefault(); //오른쪽 클릭방지
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement("a");
+  link.href = image; //href가 url이 되고
+  link.download = "PaintJs(EXPORT)"; //download가 이름이되어야한다.
+  link.click();
+}
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasCilick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color =>
@@ -80,4 +97,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
